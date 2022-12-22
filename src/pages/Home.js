03855dd79b38af9be7476bfa9845e9ec2f4a4828
Yaman Katby <m.yaman.katby@gmail.com>
2022-12-22 11:40:28 +0300

    profile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,30 @@
 import { signOut } from "firebase/auth";
 import { useCallback } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 
 const Home = () => {
+  const [user, isLoading] = useAuthState(auth);
+
   const handleSignOut = useCallback(() => {
     signOut(auth);
   }, []);
 
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div>
-      <h1>Hey, from Home component</h1>
-      <button onClick={handleSignOut}>Sign out</button>
+    <div className="max-w-md py-12 mx-auto">
+      <div className="flex items-center justify-between">
+        <div>
+          <p className="text-xl">{user.displayName}</p>
+          <p className="text-lg text-gray-700">{user.email}</p>
+        </div>
+        <button onClick={handleSignOut} className="p-4 bg-pink-400 rounded-md">
+          Sign out
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,10 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPasswrod] = useState("");
 
@@ -17,17 +18,28 @@ const SignUp = () => {
         return;
       }
 
-      createUserWithEmailAndPassword(auth, email, password).catch((e) => {
-        console.log(e);
-      });
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((auth) => {
+          updateProfile(auth.user, { displayName: name });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     },
-    [email, password]
+    [name, email, password]
   );
 
   return (
     <div className="max-w-md mx-auto py-12">
       <h1 className="text-2xl">Create new account</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
+        <input
+          type="text"
+          placeholder="Enter your name"
+          className="p-4 bg-gray-100 rounded-md"
+          value={name}
+          onChange={(e) => setName(e.currentTarget.value)}
+        />
         <input
           type="email"
           placeholder="Enter your email"
